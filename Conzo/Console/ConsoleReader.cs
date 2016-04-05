ba31f0546b3b38e4faee0528aaa920ff3e8aa69a
bouwe77 <bouwe@bouwe.nl>
2016-04-05 22:45:36 +0200

    .

---
+++ Conzo/Console/ConsoleReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conzo.Console
+{
+   internal class ConsoleReader : IConsoleReader
+   {
+      public ConsoleKey ReadFromConsole()
+      {
+         return System.Console.ReadKey(true).Key;
+      }
+   }
+}
---
+++ Conzo/Console/IConsoleWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Conzo.Console
+{
+   internal interface IConsoleWriter
+   {
+      void Initialize();
+      void WriteToConsole(string stuffToWrite);
+   }
+}
