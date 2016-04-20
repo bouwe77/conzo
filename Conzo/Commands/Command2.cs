bebed3c513b8a5eaebc976fe8610b06f20adf663
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2016-04-20 09:05:31 +0200

    .

--- Conzo/Commands/Command2.cs
+++ Conzo/Commands/Command2.cs
@@ -3,13 +3,13 @@ using Conzo.Utilities;
 
 namespace Conzo.Commands
 {
-   public class Command1 : CommandBase
+   public class Command2 : CommandBase
    {
-      public Command(Func<string> action)
+      public Command2(Func<string> action)
       {
          Action = Enforce.ArgumentNotNull(action, "action can not be null");
       }
 
-      internal Func<string> Action { get; private set; }
+      internal Func<string, ConsoleKey> Action { get; private set; }
    }
 }
