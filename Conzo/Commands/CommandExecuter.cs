30650ac5649c1b299d8226f35ce62349223308ab
bouwe77 <bouwe@bouwe.nl>
2016-11-26 14:37:11 +0100

    .

--- Conzo/Commands/CommandExecuter.cs
+++ Conzo/Commands/CommandExecuter.cs
@@ -16,37 +16,63 @@ namespace Conzo.Commands
 
       public bool TryExecute(out string commandContents)
       {
+         bool commandExecuted = false;
          commandContents = null;
 
-         bool commandExecuted = true;
-         if (_command.Condition != null)
+         bool executeCommand = CheckCondition();
+         if (executeCommand)
          {
-            commandExecuted = _command.Condition.Invoke();
+            commandExecuted = true;
+            commandContents = ExecuteCommand();
          }
 
-         if (commandExecuted)
+         return commandExecuted;
+      }
+
+      private string ExecuteCommand()
+      {
+         string commandContents;
+
+         try
+         {
+            var command1 = _command.Command as Command1;
+            if (command1 != null)
+            {
+               commandContents = command1.Action.Invoke();
+            }
+            else
+            {
+               Command2 command2 = (Command2) _command.Command;
+               commandContents = command2.Action.Invoke(_consoleKey);
+            }
+         }
+         catch
+         {
+            //TODO logging
+            throw;
+         }
+
+         return commandContents;
+      }
+
+      private bool CheckCondition()
+      {
+         bool conditionSatisfied = true;
+
+         if (_command.Condition != null)
          {
             try
             {
-               var command1 = _command.Command as Command1;
-               if (command1 != null)
-               {
-                  commandContents = command1.Action.Invoke();
-               }
-               else
-               {
-                  Command2 command2 = (Command2) _command.Command;
-                  commandContents = command2.Action.Invoke(_consoleKey);
-               }
+               conditionSatisfied = _command.Condition.Invoke();
             }
-            catch
+            catch (Exception exception)
             {
                //TODO logging
                throw;
             }
          }
 
-         return commandExecuted;
+         return conditionSatisfied;
       }
    }
 }
